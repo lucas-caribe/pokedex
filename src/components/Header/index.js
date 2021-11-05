a5a2e307fcdd743c1891bb44b7a14eb911affcc0
Lucas Caribé <lucasmoc47@gmail.com>
2021-11-05 04:52:40 -0300

    Minor changes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ import './styles.css';
 function Header() {
   return (
     <header>
-      <img src="./assets/pokemon-logo.png" alt="Pokemon Logo" />
+      <img src="/pokedex/assets/pokemon-logo.png" alt="Pokemon Logo" />
     </header>
   );
 }
